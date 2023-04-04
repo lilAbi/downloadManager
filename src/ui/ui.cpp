7c3646f15f878ddd95033d12aedf7a83fddbc926
lilAbi <105619619+lilAbi@users.noreply.github.com>
2023-04-04 00:23:34 -0500

    template for how to structure ui code

--- src/ui/ui.cpp
+++ src/ui/ui.cpp
@@ -1,2 +1,13 @@
 
 #include "ui.h"
+
+bool gfx::UserInterface::Init() {
+
+    return true;
+}
+
+
+void gfx::UserInterface::DrawUI(UI_STATE state) {
+
+
+}
--- src/ui/ui.h
+++ src/ui/ui.h
@@ -2,10 +2,36 @@
 #ifndef DOWNLOADMANAGER_UI_H
 #define DOWNLOADMANAGER_UI_H
 
+#include <unordered_map>
 
-class Ui {
 
-};
+namespace gfx {
+
+    //possible states the ui can end up in over the lifetime of the program
+    enum class UI_STATE{
+        LOGIN,
+        MAIN,
+        SETTINGS,
+    };
+
+    //something like a manager for the UI
+    class UserInterface {
+    public:
+        UserInterface() = default;
+        ~UserInterface() = default;
+
+        //TODO: build a ui cache? in the Init() function
+        //Initialize UI
+        bool Init();
+
+        //draw the current ui
+        void DrawUI(UI_STATE state);
+
+    private:
+        //state os ui
+    };
+
+}
 
 
 #endif //DOWNLOADMANAGER_UI_H
