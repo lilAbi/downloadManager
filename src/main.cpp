98f175cb897cd571fd9bc8de5262fb413f68ee6f
lilAbi <105619619+lilAbi@users.noreply.github.com>
2023-04-04 00:24:12 -0500

    Create application object and control structure to verify it can run

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,16 @@
 
+#include <core/application.h>
+#include <memory>
 
 int main() {
+
+    //create a unique ptr to an Application obj; we will be referring to this obj as the main app obj
+    std::unique_ptr<core::Application> application = std::make_unique<core::Application>();
+
+    //Initialize sub-systems; if they pass then we can run the main app loop
+    if(application->Init()){
+
+    }
 
     return 0;
 }
