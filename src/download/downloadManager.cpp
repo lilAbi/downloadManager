48f2dd9d8ae96ea68818be8a77fefe67730fb116
lilAbi <105619619+lilAbi@users.noreply.github.com>
2024-10-06 07:00:25 -0500

    starting the downloading engine

---
+++ src/download/downloadManager.cpp
@@ -0,0 +1,3 @@
+
+
+#include "downloadManager.h"
---
+++ src/download/downloadManager.h
@@ -0,0 +1,20 @@
+
+
+#ifndef DOWNLOADMANAGER_DOWNLOADMANAGER_H
+#define DOWNLOADMANAGER_DOWNLOADMANAGER_H
+
+
+//orchestrates the downloading system.
+class DownloadManager {
+public:
+    DownloadManager() = default;
+    ~DownloadManager() = default;
+private:
+    //TaskSchedular DownloadQueue;
+    //ThreadSafe Downloadabel queue;
+
+
+};
+
+
+#endif //DOWNLOADMANAGER_DOWNLOADMANAGER_H
